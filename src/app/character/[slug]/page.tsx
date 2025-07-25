0f488d9149b768cccfc6d0666e770921a631d516
salvado <pmgsalvado@protonmail.com>
2025-07-25 14:27:22 +0100

    generateStaticParams() issue solved don´t know why

--- src/app/character/[slug]/page.tsx
+++ src/app/character/[slug]/page.tsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import styles from '../character.module.css'
 
+export default async function CharacterPage({params,}:{params: Promise<{slug: string}>}){
 
-/**
-export async function generateStaticParams() {
-    const posts = await fetch('https://.../character').then((res) => res.json())
-
-    return posts.map((post) => ({
-    slug: post.slug,
-    }))
-}
- */
-
-
-export default async function CharacterPage(){
-/* export default async function CharacterPage({params,}:{params: Promise<{slug: string}>}){ */
-
-
-    //const { slug } = await params
-    //const { slug } = await generateStaticParams()
+    const { slug } = await params
+    
 
     return (
         <div className={styles.character_container}>
-            <h1>This is Characters id: {/* slug*/} </h1>
+            <h1>This is Characters id: {slug} </h1>
         </div>
     )
 }
